f9b6e7b5cc5bb1b301ed2d8ccebdae3201d7b003
Nimisha Budhwani <51719823+nimishabudhwani@users.noreply.github.com>
2019-10-27 21:20:05 -0400

    Week 2 Commit

---
+++ MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flixter
+//
+//  Created by Nimisha Budhwani on 10/27/19.
+//  Copyright © 2019 Nimisha Budhwani. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+   
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
